936093cacd60d40dd6163f226431bc793ce28460
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-07-09 22:23:50 +0330

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,8 @@
 import React from "react";
-import { ChatEngine } from "react-chat-engine";
 import "./App.css";
-import ChatFeed from "./Components/ChatFeed";
 
 const App = () => {
-  return (
-    <div>
-      <ChatEngine
-        height="100vh"
-        projectID="849f76fc-e38e-41f7-9ccc-f2ed0ba43ca8"
-        userName="pouriaaa"
-        userSecret="123456"
-        renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps}/>}
-        onConnect={(creds) => console.log('creds')}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/Components/MessageForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MessageForm = () => {
-  return (
-    <div>MessageForm</div>
-  )
-}
-
-export default MessageForm
