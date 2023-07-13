6284f30f082de5aaab13bae29cd861a9f035185d
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-07-13 23:23:07 +0330

    working on chats Info

---
+++ src/Components/Chats.jsx
@@ -0,0 +1,52 @@
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "./Context/AuthContext";
+
+const Chats = ({ showChatHandler }) => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+        console.log(chats);
+      });
+
+      return () => {
+        unsub();
+      };
+    };
+
+    currentUser?.uid && getChats();
+    console.log(Object.entries(chats));
+  }, [currentUser.uid]);
+
+  return (
+    <div>
+      <p className="font-bold mt-2 text-sm p-2">chats</p>
+      <ul className="w-full py-2 gap-3 flex flex-col">
+        {Object.entries(chats)?.map((chat) => (
+          <li
+            key={chat[0]}
+            onClick={() => showChatHandler()}
+            className="cursor-pointer w-full  bg-white h-14 gap-2 flex items-center px-2 rounded-2xl"
+          >
+            <img className="rounded-full h-10 " src={chat[1].userInfo.photoURL} alt="" />
+            <div className="h-full py-2 w-3/4 justify-around flex flex-col ">
+              <p className="text-start text-xs font-bold truncate">
+                {chat[1].userInfo.displayName}
+              </p>
+              <p className="text-start text-xs text-gray-500 truncate">
+                {chat[1].userInfo.lastMessage?.text}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Chats;
--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,31 +1,14 @@
 import React from "react";
 import NavBar from "./NavBar";
 import SearchBar from "./SearchBar";
+import Chats from "./Chats";
 
 const SideBar = ({ showChatHandler }) => {
   return (
     <div className="md:w-[260px] xl:w-[300px] max-sm:w-full sm:w-full h-full bg-[#f8f8f8] rounded-l-2xl p-4 overflow-y-auto">
       <NavBar />
       <SearchBar />
-      <p className="font-bold mt-2 text-sm p-2">chats</p>
-      <ul className="w-full py-2 gap-3 flex flex-col">
-        <li
-          onClick={() => showChatHandler()}
-          className="cursor-pointer w-full  bg-white h-14 gap-2 flex items-center px-2 rounded-2xl"
-        >
-          <img
-            className="rounded-full h-10 "
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1GOS8Mxd73Bu4zfCIWmou11x4QkGp2cFf7w&usqp=CAU"
-            alt=""
-          />
-          <div className="h-full py-2 w-3/4 justify-around flex flex-col ">
-            <p className="text-start text-xs font-bold truncate">Tyler</p>
-            <p className="text-start text-xs text-gray-500 truncate">
-              Hi i want to go to gym at 6 o'clock when can you come ?
-            </p>
-          </div>
-        </li>
-      </ul>
+      <Chats showChatHandler={showChatHandler}/>
     </div>
   );
 };
