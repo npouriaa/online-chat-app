0c864d8a0ee3842e5ed1967b559675f4ad7d75b8
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-07-22 12:29:03 +0330

    change

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -5,7 +5,7 @@ import Chats from "./Chats";
 
 const SideBar = ({ showChatHandler }) => {
   return (
-    <div className="md:w-[260px] xl:w-[300px] 2xl:w-[400px] max-sm:w-full sm:w-full h-full bg-[#f8f8f8] rounded-l-2xl p-4 overflow-y-auto">
+    <div className="md:w-[260px] xl:w-[300px] 2xl:w-[400px] max-sm:w-full sm:w-full h-full bg-[#f8f8f8] p-4 overflow-y-auto">
       <NavBar />
       <SearchBar />
       <Chats showChatHandler={showChatHandler}/>
